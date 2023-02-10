b46a4233fbf6b789686ade9312d29435f7e8445c
DanMarzo <marzogildan@gmail.com>
2023-02-10 16:11:03 -0300

    Entidade referente aos medicamentos da prescrição do Paciente (PrescricacaoPaciente)

---
+++ WEB_API_HealTime/Models/PrescricaoMedicamento.cs
@@ -0,0 +1,23 @@
+
+using System.Text.Json.Serialization;
+
+namespace WEB_API_HealTime.Models;
+
+public class PrescricaoMedicamento
+{
+    public int PrescricaoMedicamentoId { get; set; }
+    public int? PrescricaoPacienteId { get; set; }
+    [JsonIgnore]
+    public PrescricaoPaciente PrescricaoPaciente { get; set; }
+    /*public int AndamentoMedicacaoId { get; set; }
+    [JsonIgnore]
+    public AndamentoMedicacao AndamentoMedicacao { get; set; }*/
+    /*public int MedicacaoId { get; set; }
+    [JsonIgnore]
+    public Medicacao Medicacao { get; set; }*/
+    public DateTime? HrDtMedicacao { get; set; }
+    public DateTime? DtTerminoTratamento { get; set; }
+    public int? QtdDiariaMedia { get; set; }
+    public bool CheckSituacao { get; set; }
+
+}
