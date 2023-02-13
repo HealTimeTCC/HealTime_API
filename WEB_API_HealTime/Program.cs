8d5fd521c98bdaa3eb45e0e969d69c16e614f823
DanMarzo <marzogildan@gmail.com>
2023-02-13 10:27:51 -0300

    Apagando espaço desnecessario

--- WEB_API_HealTime/Program.cs
+++ WEB_API_HealTime/Program.cs
@@ -21,9 +21,6 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("rrsoft"));
 });
 var app = builder.Build();
-
-
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
