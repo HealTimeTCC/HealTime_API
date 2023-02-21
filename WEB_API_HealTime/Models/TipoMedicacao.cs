9fdabc53bd146719ca93d4e650ccfdcc59359719
DanMarzo <marzogildan@gmail.com>
2023-02-21 17:38:29 -0300

    Criando campo para a classe de remedio e o tipo de aplicação

--- WEB_API_HealTime/Models/TipoMedicacao.cs
+++ WEB_API_HealTime/Models/TipoMedicacao.cs
@@ -1,11 +1,14 @@
 
 using Newtonsoft.Json;
+using WEB_API_HealTime.Models.Enuns;
 
 namespace WEB_API_HealTime.Models;
 
 public class TipoMedicacao
 {
     public int TipoMedicacaoId { get; set; }
+    public string TituloTipoMedicacao { get; set; }
+    public ClasseAplicacao ClasseAplicacao { get; set; }
     public string DescMedicacao { get; set; }
     [JsonIgnore]
     public Medicacao Medicacao { get; set; }
