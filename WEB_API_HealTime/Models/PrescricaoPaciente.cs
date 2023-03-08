c3f39f357f32931294587bbc1a37d752a5caaca0
DanMarzo <marzogildan@gmail.com>
2023-03-07 22:46:41 -0300

    Incluindo null

--- WEB_API_HealTime/Models/PrescricaoPaciente.cs
+++ WEB_API_HealTime/Models/PrescricaoPaciente.cs
@@ -9,7 +9,7 @@ public class PrescricaoPaciente
     [JsonIgnore]
     public Medico Medico { get; set; }
     public int PacienteId { get; set; }
-    public DateTime CriadoEm { get; set; }
+    public DateTime? CriadoEm { get; set; }
     public DateTime Emissao { get; set; }
     public DateTime Validade { get; set; }
     public string DescFichaPessoa{ get; set; }
