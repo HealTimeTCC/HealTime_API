b048aaa8b28ffa96380bafa820a413b14abffae3
DanMarzo <marzogildan@gmail.com>
2023-03-07 21:17:58 -0300

    Nova Model PrescricaoPaciente

--- WEB_API_HealTime/Models/PrescricaoPaciente.cs
+++ WEB_API_HealTime/Models/PrescricaoPaciente.cs
@@ -1,9 +1,13 @@
-﻿namespace WEB_API_HealTime.Models;
+﻿using Newtonsoft.Json;
+
+namespace WEB_API_HealTime.Models;
 
 public class PrescricaoPaciente
 {
     public int PrescricaoPacienteId { get; set; }
     public int MedicoId { get; set; }
+    [JsonIgnore]
+    public Medico Medico { get; set; }
     public int PacienteId { get; set; }
     public DateTime CriadoEm { get; set; }
     public DateTime Emissao { get; set; }
