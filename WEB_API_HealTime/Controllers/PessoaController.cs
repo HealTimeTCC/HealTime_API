83ae2a62b9ff5a3acfe5427282a92bf4b3a0c02d
DanMarzo <marzogildan@gmail.com>
2023-03-11 14:32:32 -0300

    Controller de registro e gerador de tokens

--- WEB_API_HealTime/Controllers/PessoaController.cs
+++ WEB_API_HealTime/Controllers/PessoaController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using WEB_API_HealTime.Data;
+using WEB_API_HealTime.Models;
 
 namespace WEB_API_HealTime.Controllers;
 
+[Authorize]
 [Route("[controller]")]
 [ApiController]
 public class PessoaController : ControllerBase
@@ -14,5 +21,45 @@ public class PessoaController : ControllerBase
     {
         _context = context;
         _configuration = configuration;
+    }
+    private string CriarToken(Pessoa pessoa)
+    {
+        List<Claim> claims = new List<Claim>
+        {
+            new Claim (ClaimTypes.NameIdentifier, pessoa.PessoaId.ToString()),
+            new Claim (ClaimTypes.Name, pessoa.NomePessoa),
+            new Claim (ClaimTypes.Surname, pessoa.SobreNomePessoa),
+            new Claim (ClaimTypes.UserData, pessoa.TipoPessoaId.ToString())
+        };
+        SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8
+            .GetBytes(_configuration.GetSection("ConfigurationToken:Key").Value));
+        SigningCredentials credentials =
+            new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+        
+        SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddHours(1),
+            SigningCredentials = credentials
+        };
+        JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+        SecurityToken token = tokenHandler.CreateToken (tokenDescriptor);
+        return tokenHandler.WriteToken (token);
+    }
+
+    [HttpPost("Registro")]
+    public async Task<IActionResult> RegistraPessoa(Pessoa pessoa)
+    {
+        try
+        {
+
+            _context.Pessoas.Add (pessoa);
+            _context.SaveChanges();
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);  
+        }
     }
 }
