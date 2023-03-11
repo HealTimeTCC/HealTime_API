84519fec3b1feb92cc2202d59590ee385d276f2f
DanMarzo <marzogildan@gmail.com>
2023-03-11 20:33:41 -0300

    Nova entidade a ser formada: Medicacao

---
+++ WEB_API_HealTime/Models/Medicacao.cs
@@ -0,0 +1,12 @@
+﻿namespace WEB_API_HealTime.Models;
+
+public class Medicacao
+{
+    public int MedicacaoId { get; set; }
+    public int StatusMedicacaoId { get; set; }
+    public int TipoMedicacaoId { get; set; }
+    public string NomeMedicacao { get; set; }
+    public string CompostoAtivoMedicacao { get; set; }
+    public string LaboratorioMedicaocao { get; set; }
+    public string Generico { get; set; }
+}
