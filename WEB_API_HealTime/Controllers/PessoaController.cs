332868e609f1273ab8124b6229e4ed813132c422
DanMarzo <marzogildan@gmail.com>
2023-03-11 13:49:08 -0300

    Nova controller responsavel pelo controle de pessoas, nesse mesmo commit ja foi feito a injeção de dependencia com o DataContext e IConfiguration

---
+++ WEB_API_HealTime/Controllers/PessoaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WEB_API_HealTime.Data;
+
+namespace WEB_API_HealTime.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class PessoaController : ControllerBase
+{
+    private readonly DataContext _context;
+    private readonly IConfiguration _configuration;
+    public PessoaController(DataContext context, IConfiguration configuration)
+    {
+        _context = context;
+        _configuration = configuration;
+    }
+}
