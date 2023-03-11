3f93dab41e6269216a51368db4777252e4a88f80
DanMarzo <marzogildan@gmail.com>
2023-03-11 14:33:23 -0300

    Props responsaveis de receber a senha Criptograda

--- WEB_API_HealTime/Models/Pessoa.cs
+++ WEB_API_HealTime/Models/Pessoa.cs
@@ -9,6 +9,10 @@ public class Pessoa
     public string CpfPessoa { get; set; }
     public string NomePessoa { get; set; }
     public string SobreNomePessoa { get; set; }
+    [NotMapped]
+    public string PasswordString { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
     public DateTime DtNascPessoa { get; set; }
     [NotMapped]
     public string TokenJwt { get; set; }
