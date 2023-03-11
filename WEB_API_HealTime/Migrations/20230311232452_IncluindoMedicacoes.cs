898341867074c1ef60b4df3666bd05e04432dbce
DanMarzo <marzogildan@gmail.com>
2023-03-11 20:34:36 -0300

    Migrations responsaveis por incluir a Table medicacoes

---
+++ WEB_API_HealTime/Migrations/20230311232452_IncluindoMedicacoes.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WEB_API_HealTime.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncluindoMedicacoes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Pessoas",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Pessoas",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Medicacoes",
+                columns: table => new
+                {
+                    MedicacaoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StatusMedicacaoId = table.Column<int>(type: "int", nullable: false),
+                    TipoMedicacaoId = table.Column<int>(type: "int", nullable: false),
+                    NomeMedicacao = table.Column<string>(type: "VARCHAR(80)", nullable: false),
+                    CompostoAtivoMedicacao = table.Column<string>(type: "VARCHAR(50)", nullable: false),
+                    LaboratorioMedicaocao = table.Column<string>(type: "VARCHAR(80)", nullable: false),
+                    Generico = table.Column<string>(type: "CHAR(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicacaoId", x => x.MedicacaoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medicacoes");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Pessoas",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Pessoas",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+        }
+    }
+}
