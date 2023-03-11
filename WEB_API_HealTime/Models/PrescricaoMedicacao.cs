4648dffab356ecaa0c20a9d2501fd05421e22ae0
DanMarzo <marzogildan@gmail.com>
2023-03-11 20:34:11 -0300

    Nova entidade: PrescricaoMedicamento, ela depende de medicacoes e prescricaoPaciente

---
+++ WEB_API_HealTime/Models/PrescricaoMedicacao.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WEB_API_HealTime.Models;
+
+public class PrescricaoMedicacao
+{
+    public int PrescricaoMedicacaoId { get; set; }
+    [JsonIgnore]
+    public PrescricaoPaciente PrescricaoPacienteId { get; set; }
+    public int MedicacaoId { get; set; }
+    [JsonIgnore]
+    public Medicacao Medicacao { get; set; }
+    public int Qtde { get; set; }//perguntar como fica os liquidos
+    public int Intervalo { get; set; }
+    public int Duracao { get; set; }//Por interpretamos como duracao de dias
+}
