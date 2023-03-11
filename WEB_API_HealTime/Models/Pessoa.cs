85fa1a68ca5c40a82f03db375de1f6200ee32c6f
DanMarzo <marzogildan@gmail.com>
2023-03-11 19:08:50 -0300

    Nullable para usar o json

--- WEB_API_HealTime/Models/Pessoa.cs
+++ WEB_API_HealTime/Models/Pessoa.cs
@@ -1,19 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WEB_API_HealTime.Models;
 
 public class Pessoa
 {
     public int PessoaId { get; set; }
-    public int TipoPessoaId { get; set; }
+    public int? TipoPessoaId { get; set; }
     public string CpfPessoa { get; set; }
     public string NomePessoa { get; set; }
     public string SobreNomePessoa { get; set; }
     [NotMapped]
     public string PasswordString { get; set; }
+    [JsonIgnore]
     public byte[] PasswordHash { get; set; }
+    [JsonIgnore]
     public byte[] PasswordSalt { get; set; }
-    public DateTime DtNascPessoa { get; set; }
+    public DateTime? DtNascPessoa { get; set; }
     [NotMapped]
     public string TokenJwt { get; set; }
 }
