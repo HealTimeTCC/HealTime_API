aca15f475734e6b22124f6133467d33c62833c57
DanMarzo <marzogildan@gmail.com>
2023-03-10 22:22:23 -0300

    Incluindo packages para JWT

--- WEB_API_HealTime/Controllers/PrescricaoPacienteController.cs
+++ WEB_API_HealTime/Controllers/PrescricaoPacienteController.cs
@@ -12,34 +12,39 @@ namespace WEB_API_HealTime.Controllers;
 [ApiController]
 public class PrescricaoPacienteController : ControllerBase
 {
-    private readonly DataContext _context ;
+    private readonly DataContext _context;
     public PrescricaoPacienteController(DataContext context) { _context = context; }
 
     [HttpPost]
-    public async Task<IActionResult> IncluiPrescricaoAsync([FromBody]DtoPrescricaoPaciente prescricaoPaciente)
+    public async Task<IActionResult> IncluiPrescricaoAsync([FromBody] DtoPrescricaoPaciente prescricaoPaciente)
     {
-		try
-		{
-			Medico medico = prescricaoPaciente is null ?
-				throw new Exception("Objeto nulo")
-				: await _context.Medicos
-				.FirstOrDefaultAsync(x => x.MedicoId == prescricaoPaciente.MedicoId);
-			PrescricaoPaciente insertPrescricaoPaciente = new()
-			{
-				CriadoEm = DateTime.Now,
-				MedicoId = prescricaoPaciente.MedicoId,
-				Emissao = prescricaoPaciente.Emissao,
-				PacienteId = prescricaoPaciente.PacienteId,
-				Validade = prescricaoPaciente.Validade,
-				DescFichaPessoa = prescricaoPaciente.DescFichaPessoa
-			};
-			await _context.PrescricaoPacientes.AddAsync(insertPrescricaoPaciente);
-			await _context.SaveChangesAsync();
-			return Ok();
-		}
-		catch (Exception ex)
-		{
-			return BadRequest(ex.Message);
-		}
+        try
+        {
+            Medico medico = prescricaoPaciente is null ?
+                throw new Exception("Objeto nulo")
+                : await _context.Medicos
+                .FirstOrDefaultAsync(x => x.MedicoId == prescricaoPaciente.MedicoId);
+            PrescricaoPaciente insertPrescricaoPaciente = new()
+            {
+                CriadoEm = DateTime.Now,
+                MedicoId = prescricaoPaciente.MedicoId,
+                Emissao = prescricaoPaciente.Emissao,
+                PacienteId = prescricaoPaciente.PacienteId,
+                Validade = prescricaoPaciente.Validade,
+                DescFichaPessoa = prescricaoPaciente.DescFichaPessoa
+            };
+            await _context.PrescricaoPacientes.AddAsync(insertPrescricaoPaciente);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
     }
+    //[HttpGet("LeituraDescricao")]
+    //public async Task<IActionResult> LeituraDescricaoAsync()
+    //{
+
+    //}
 }
