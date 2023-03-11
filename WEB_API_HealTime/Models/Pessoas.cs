df69c4de3211647a13c23421f13c26e7d9452824
DanMarzo <marzogildan@gmail.com>
2023-03-11 12:54:19 -0300

    Classe Pessoa, para mapamento EF Core

--- WEB_API_HealTime/Models/Pessoas.cs
+++ WEB_API_HealTime/Models/Pessoas.cs
@@ -1,6 +1,11 @@
-﻿namespace WEB_API_HealTime.Models
+﻿namespace WEB_API_HealTime.Models;
+
+public class Pessoas
 {
-    public class Pessoas
-    {
-    }
+    public int PessoaId { get; set; }
+    public int TipoPessoaId { get; set; }
+    public string CpfPessoa { get; set; }
+    public string NomePessoa { get; set; }
+    public string SobreNomePessoa { get; set; }
+    public DateTime DtNascPessoa { get; set; }
 }
