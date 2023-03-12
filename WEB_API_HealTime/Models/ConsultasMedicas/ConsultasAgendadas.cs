cbe8cb176ea3e3e8bd2e079daea1caa6c2960eb5
DanMarzo <marzogildan@gmail.com>
2023-03-11 21:54:08 -0300

    Arrumando por pastas

---
+++ WEB_API_HealTime/Models/ConsultasMedicas/ConsultasAgendadas.cs
@@ -0,0 +1,14 @@
+﻿namespace WEB_API_HealTime.Models.ConsultasMedicas;
+
+public class ConsultasAgendadas
+{
+    public int ConsultasAgendadasId { get; set; }
+    public int StatusConsultasId { get; set; }
+    public int Especialidade { get; set; }
+    public int PacienteId { get; set; }
+    public int MedicoId { get; set; }
+    public DateTime DataSolicitacaoConsulta { get; set; }
+    public DateTime DataConsulta { get; set; }
+    public string MotivoConsulta { get; set; }
+    public string Encaminhamento { get; set; }
+}
---
+++ WEB_API_HealTime/Models/ConsultasMedicas/Medico.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using WEB_API_HealTime.Models.Medicacoes;
+
+namespace WEB_API_HealTime.Models.ConsultasMedicas;
+
+public class Medico
+{
+    public int MedicoId { get; set; }
+    public string NmMedico { get; set; }
+    public int CrmMedico { get; set; }
+    public string UfCrmMedico { get; set; }
+    [JsonIgnore]
+    public PrescricaoPaciente PrescricaoPaciente { get; set; }
+}
