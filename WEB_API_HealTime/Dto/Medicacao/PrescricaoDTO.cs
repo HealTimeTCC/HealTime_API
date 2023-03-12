6e4ab012040399f6b4783fcf2175660e9726a712
DanMarzo <marzogildan@gmail.com>
2023-03-11 22:27:54 -0300

    Dto recebendo 3 paramentro para inclusao de Prescricao

---
+++ WEB_API_HealTime/Dto/Medicacao/PrescricaoDTO.cs
@@ -0,0 +1,10 @@
+﻿using WEB_API_HealTime.Models.ConsultasMedicas;
+using WEB_API_HealTime.Models.Medicacoes;
+
+namespace WEB_API_HealTime.Dto.PrescricaoDTO;
+public class PrescricaoDTO
+{
+    public PrescricaoPaciente PrescricaoPaciente { get; set; }
+    public List<PrescricaoMedicacao> PrescricoesMedicacoes { get; set; }
+    public Medico Medico { get; set; }
+}
