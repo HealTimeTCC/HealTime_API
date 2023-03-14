e92bd5088272fbca0c190ea1430fe9afe67eb4fd
DanMarzo <marzogildan@gmail.com>
2023-03-14 19:56:22 -0300

    Organizando arquivos

--- Querry/querry.sql
+++ Querry/querry.sql
@@ -1,10 +1,10 @@
 select * from PrescricaoPacientes
 SELECT * FROM Medicacoes
 select * from PrescricoesMedicacoes
-
+select * from Pessoas
 SELECT * FROM TiposMedicacoes
 
 
-DELETE FROM PrescricaoPacientes
-DELETE FROM Medicacoes
-DELETE FROM PrescricoesMedicacoes
+--DELETE FROM PrescricaoPacientes
+--DELETE FROM Medicacoes
+--DELETE FROM PrescricoesMedicacoes
