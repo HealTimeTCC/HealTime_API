dd81576891e4f53eba7dd67b9100c8248139b8be
DanMarzo <marzogildan@gmail.com>
2023-03-14 19:14:35 -0300

    Consertando ignore

---
+++ database_diagram/querry.sql
@@ -0,0 +1,10 @@
+select * from PrescricaoPacientes
+SELECT * FROM Medicacoes
+select * from PrescricoesMedicacoes
+
+SELECT * FROM TiposMedicacoes
+
+
+DELETE FROM PrescricaoPacientes
+DELETE FROM Medicacoes
+DELETE FROM PrescricoesMedicacoes
