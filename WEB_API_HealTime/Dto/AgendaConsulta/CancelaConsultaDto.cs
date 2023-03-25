8b1671a97330e69721f901011b0d09011c881d3b
DanMarzo <marzogildan@gmail.com>
2023-03-25 19:09:25 -0300

    Alterações no DTO, afim de receberm ids, e coisas mais simples

---
+++ WEB_API_HealTime/Dto/AgendaConsulta/CancelaConsultaDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WEB_API_HealTime.Dto.AgendaConsulta;
+
+[NotMapped]
+public class CancelaConsultaDto
+{
+    public int IdConsulta { get; set; }
+    public string MotivoCancelamento { get; set; }
+    public DateTime DataCancela { get; set; }
+}
