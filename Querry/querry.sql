f2faeaa642fed44ddc23b554140f0d5763fd28d8
DanMarzo <marzogildan@gmail.com>
2023-03-25 12:09:57 -0300

    Tipos default tipo medicacao

--- Querry/querry.sql
+++ Querry/querry.sql
@@ -11,10 +11,21 @@ PM.PrescricaoPacienteId
 FROM PrescricoesMedicacoes PM
 INNER JOIN Medicacoes M ON PM.MedicacaoId = M.MedicacaoId
 
-
+select * from TiposMedicacoes
 
 sp_help Medicacoes
+sp_help Medicos
 
+insert into Medicos(CrmMedico,NmMedico, UfCrmMedico) values('123456', 'Dan Marzo', 'UF')
+
+delete FROM Medicacoes
+
+
+
+
+SELECT * FROM Medicacoes
+SELECT * FROM PrescricaoPacientes
+SELECT * FROM PrescricoesMedicacoes
 SELECT * FROM Pessoas
 SELECT * FROM TiposMedicacoes
 SELECT * FROM Medicos
