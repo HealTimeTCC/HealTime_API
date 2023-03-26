10298846e4c12084bf9893981c81c7dccaa592cf
DanMarzo <marzogildan@gmail.com>
2023-03-26 18:39:43 -0300

    Iniciando o uso do HttpContext

--- WEB_API_HealTime/Controllers/PessoaController.cs
+++ WEB_API_HealTime/Controllers/PessoaController.cs
@@ -8,6 +8,7 @@ using System.Security.Claims;
 using System.Text;
 using WEB_API_HealTime.Data;
 using WEB_API_HealTime.Models.Pessoas;
+using WEB_API_HealTime.Dto.Pessoa;
 
 namespace WEB_API_HealTime.Controllers;
 
@@ -18,10 +19,12 @@ public class PessoaController : ControllerBase
 {
     private readonly DataContext _context;
     private readonly IConfiguration _configuration;
-    public PessoaController(DataContext context, IConfiguration configuration)
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    public PessoaController(DataContext context, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
     {
         _context = context;
         _configuration = configuration;
+        _httpContextAccessor = httpContextAccessor;
     }
     private string CriarToken(Pessoa pessoa)
     {
@@ -30,7 +33,7 @@ public class PessoaController : ControllerBase
             new Claim (ClaimTypes.NameIdentifier, pessoa.PessoaId.ToString()),
             new Claim (ClaimTypes.Name, pessoa.NomePessoa),
             new Claim (ClaimTypes.Surname, pessoa.SobreNomePessoa),
-            new Claim (ClaimTypes.UserData, pessoa.TipoPessoaId.ToString())
+            new Claim (ClaimTypes.UserData, pessoa.TipoPessoa.ToString())
         };
         SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8
             .GetBytes(_configuration.GetSection("ConfigurationToken:Key").Value));
@@ -47,6 +50,15 @@ public class PessoaController : ControllerBase
         SecurityToken token = tokenHandler.CreateToken (tokenDescriptor);
         return tokenHandler.WriteToken (token);
     }
+
+    //new Claim(ClaimTypes.NameIdentifier, pessoa.PessoaId.ToString()),
+    //new Claim(ClaimTypes.Name, pessoa.NomePessoa),
+    //new Claim(ClaimTypes.Surname, pessoa.SobreNomePessoa),
+    //new Claim(ClaimTypes.UserData, pessoa.TipoPessoaId.ToString())
+
+    
+    //private 
+
     [AllowAnonymous]
     [HttpPost("Registro")]
     public async Task<IActionResult> RegistraPessoaAsync(Pessoa pessoa)
@@ -69,12 +81,12 @@ public class PessoaController : ControllerBase
     }
     [AllowAnonymous]
     [HttpPost("Autenticar")]
-    public async Task<IActionResult> AutenticarAsync(Pessoa pessoa)
+    public async Task<IActionResult> AutenticarAsync(LoginPasswordDto pessoa)
     {
         try
         {
             Pessoa pessoaPesquisa = await _context.Pessoas
-                .FirstOrDefaultAsync(user => user.NomePessoa.ToUpper() == pessoa.NomePessoa.ToUpper());
+                .FirstOrDefaultAsync(user => user.NomePessoa.ToUpper() == pessoa.Email.ToUpper());
             if (pessoaPesquisa is null)
                 throw new Exception("User nulo");
             else if (!Criptografia.VerificarPasswordHash(pessoa.PasswordString, pessoaPesquisa.PasswordHash, pessoaPesquisa.PasswordSalt))
