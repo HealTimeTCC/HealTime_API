9a08ba2833ddb90cff3c526a16a314bdfd7cb8ed
DanMarzo <marzogildan@gmail.com>
2023-03-26 15:01:44 -0300

    Arrumando nomes utilizados na dto, para seguir com o padrao do resto

--- WEB_API_HealTime/Dto/AgendaConsulta/CancelaConsultaDto.cs
+++ WEB_API_HealTime/Dto/AgendaConsulta/CancelaConsultaDto.cs
@@ -5,7 +5,8 @@ namespace WEB_API_HealTime.Dto.AgendaConsulta;
 [NotMapped]
 public class CancelaConsultaDto
 {
-    public int IdConsulta { get; set; }
+    public int ConsultaId { get; set; }
+    public int PacienteId { get; set; }
     public string MotivoCancelamento { get; set; }
     public DateTime DataCancela { get; set; }
 }
---
+++ WEB_API_HealTime/Dto/AgendaConsulta/MedicoDto.cs
@@ -0,0 +1,15 @@
+﻿using WEB_API_HealTime.Models.ConsultasMedicas;
+
+namespace WEB_API_HealTime.Dto.AgendaConsulta;
+
+
+public class MedicoDto
+{
+    public int MedicoId { get; set; }
+    public Medico Medico { get; set; }
+    public int PacienteId { get; set; }
+    public DateTime CriadoEm { get; set; }
+    public DateTime Emissao { get; set; }
+    public DateTime Validade { get; set; }
+    public string DescFichaPessoa { get; set; }
+}
