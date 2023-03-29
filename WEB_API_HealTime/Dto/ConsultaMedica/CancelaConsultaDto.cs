50f4513d538a2844af53052ae13e050bc058d777
DanMarzo <marzogildan@gmail.com>
2023-03-28 22:41:58 -0300

    Novas dtos para controller

---
+++ WEB_API_HealTime/Dto/ConsultaMedica/CancelaConsultaDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WEB_API_HealTime.Dto.AgendaConsulta;
+
+[NotMapped]
+public class CancelaConsultaDto
+{
+    public int ConsultaId { get; set; }
+    public int PacienteId { get; set; }
+    public string MotivoCancelamento { get; set; }
+    public DateTime DataCancela { get; set; }
+}
---
+++ WEB_API_HealTime/Dto/ConsultaMedica/ListConsultasDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WEB_API_HealTime.Dto.AgendaConsulta;
+
+public class ListConsultasDTO
+{
+    [Required]
+    public int PacienteId { get; set; }
+    [Required]
+    public int StatusConsultaId { get; set; }
+}
