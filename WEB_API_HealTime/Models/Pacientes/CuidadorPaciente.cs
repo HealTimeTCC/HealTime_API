16aae0166e25cb6b7548e79a2ae4e329f4d1dd86
DanMarzo <marzogildan@gmail.com>
2023-04-02 12:10:32 -0300

    Nova entidade cuidador pessoa

---
+++ WEB_API_HealTime/Models/Pacientes/CuidadorPaciente.cs
@@ -0,0 +1,13 @@
+﻿using WEB_API_HealTime.Models.Pessoas;
+
+namespace WEB_API_HealTime.Models.Pacientes;
+
+public class CuidadorPaciente
+{
+    public int PacienteId { get; set; }
+    public Pessoa PessoaPaciente { get; set; }
+    public int CuidadorId { get; set; }
+    public Pessoa PessoaCuidador { get; set; }
+    public DateTime CriadoEm { get; set; }
+    public DateTime FinalizadoEm { get; set; }
+}
