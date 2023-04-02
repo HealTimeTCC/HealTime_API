756508d4075ee875ffa8bf487b620536fedf3f0e
DanMarzo <marzogildan@gmail.com>
2023-04-02 19:04:50 -0300

    Incluindo novos contratos

--- WEB_API_HealTime/Repository/Interfaces/IPessoaRepository.cs
+++ WEB_API_HealTime/Repository/Interfaces/IPessoaRepository.cs
@@ -4,5 +4,8 @@ namespace WEB_API_HealTime.Repository.Interfaces;
 
 public interface IPessoaRepository
 {
-    Task<string> IncluiPessoa(Pessoa pessoa);
+    Task<string> IncluiPessoa(Pessoa pessoa, ContatoPessoa contatoPessoa = null);
+    Task<Pessoa> AutenticarPessoas(string email);
+    Task<Pessoa> ConsultarPessoa(string dadoConsulta, TipoConsultaPessoa tipoConsultaPessoa);
+    Task<bool> UpdatePessoa(Pessoa pessoa, TipoUpdatePessoa tipoConsultaPessoa);
 }
