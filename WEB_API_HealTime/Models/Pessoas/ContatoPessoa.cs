6ea1eeb0051dcc2460912d95e1c27a4d1e03bafa
DanMarzo <marzogildan@gmail.com>
2023-04-02 19:03:27 -0300

    Table para contatos, e exclusao do tipo de contato

--- WEB_API_HealTime/Models/Pessoas/ContatoPessoa.cs
+++ WEB_API_HealTime/Models/Pessoas/ContatoPessoa.cs
@@ -1,20 +1,19 @@
 ﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 using WEB_API_HealTime.Models.Pessoas.Enums;
 
 namespace WEB_API_HealTime.Models.Pessoas;
 
 public class ContatoPessoa
 {
-    public int ContatoPessoaId { get; set; }
     public int PessoaId { get; set; }
     [JsonIgnore]
     public Pessoa Pessoa { get; set; }
     public string Email { get; set; }
     public DateTime CriadoEm { get; set; }
-    public DateTime AtualizadoEm { get; set; }
+    public DateTime? AtualizadoEm { get; set; }
     public string Telefone { get; set; }
     public string TelefoneSecundario { get; set; }
     public string Celular { get; set; }
-    public EnumTipoContato TipoContato { get; set; }
-
+    public string CelularSecundario { get; set; }
 }
--- WEB_API_HealTime/Models/Pessoas/Enums/EnumTipoContato.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WEB_API_HealTime.Models.Pessoas.Enums;
-
-public enum EnumTipoContato
-{
-    nada = 1
-}
