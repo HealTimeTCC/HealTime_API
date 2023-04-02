8570370222d426db087f4d91aeb731144710cad2
DanMarzo <marzogildan@gmail.com>
2023-04-02 19:08:33 -0300

    Adicionando novos enum para verificação de tamanho de strings

---
+++ WEB_API_HealTime/Utility/EnumsGlobal/TipoVerificadorCaracteresMinimos.cs
@@ -0,0 +1,12 @@
+﻿namespace WEB_API_HealTime.Utility.Enums;
+
+public enum TipoVerificadorCaracteresMinimos
+{
+    Nome = 1,
+    Endereco = 2,
+    MotivoCancelamentoConsulta = 3,
+    UF = 4,
+    CRM = 5,
+    Telefone = 6,
+    Celular = 7
+}
