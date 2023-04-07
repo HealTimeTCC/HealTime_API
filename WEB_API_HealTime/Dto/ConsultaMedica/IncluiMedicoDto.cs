4b0342d8b5de565dd3b734396bdb306d0717e5d8
DanMarzo <marzogildan@gmail.com>
2023-04-06 23:11:20 -0300

    Novo Dto para incluir medico

---
+++ WEB_API_HealTime/Dto/ConsultaMedica/IncluiMedicoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_API_HealTime.Dto.ConsultaMedica;
+
+public class IncluiMedicoDto
+{
+    public string NmMedico { get; set; }
+    public string CrmMedico { get; set; }
+    public int CodigoIgbeUfCrmMedico { get; set; }
+}
+
