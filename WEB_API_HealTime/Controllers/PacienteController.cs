15314d8841788d3995ebe180cb5b1905bc2bd2bb
DanMarzo <marzogildan@gmail.com>
2023-04-09 19:03:22 -0300

    Correção de nomes, e inclusao de cuidador paciente para testes

--- WEB_API_HealTime/Controllers/PacienteController.cs
+++ WEB_API_HealTime/Controllers/PacienteController.cs
@@ -73,9 +73,9 @@ public class PacienteController : ControllerBase
 
             if (pacienteResponsavelDto.PacienteCpf != null && pacienteResponsavelDto.PacienteId != null)
             {
-                if (!FormataDados.VerificadorCpfPessoa(pacienteResponsavelDto.ResponsavelCpf))
+                if (!FormataDados.VerificadorCpfPessoa(pacienteResponsavelDto.PacienteCpf))
                     return BadRequest("CPF inválido");
-                using (Pessoa paciente = await _pessoasRepository.ConsultarPessoa(tipoConsultaPessoa: TipoConsultaPessoa.cpfEIdPessoa, cpfConsulta: pacienteResponsavelDto.ResponsavelCpf, idPessoa: pacienteResponsavelDto.ResponsavelId.ToString()))
+                using (Pessoa paciente = await _pessoasRepository.ConsultarPessoa(tipoConsultaPessoa: TipoConsultaPessoa.cpfEIdPessoa, cpfConsulta: pacienteResponsavelDto.PacienteCpf, idPessoa: pacienteResponsavelDto.PacienteId.ToString()))
                 {
                     if (paciente is null || paciente.TipoPessoa != EnumTipoPessoa.PacienteIncapaz)
                         return NotFound("paciente com essas caracteristicas não encontrado(a)");
@@ -85,9 +85,9 @@ public class PacienteController : ControllerBase
             }
             else if (pacienteResponsavelDto.PacienteCpf != null && pacienteResponsavelDto.PacienteId == null)
             {
-                if (!FormataDados.VerificadorCpfPessoa(pacienteResponsavelDto.ResponsavelCpf))
+                if (!FormataDados.VerificadorCpfPessoa(pacienteResponsavelDto.PacienteCpf))
                     return BadRequest("CPF inválido");
-                using (Pessoa paciente = await _pessoasRepository.ConsultarPessoa(tipoConsultaPessoa: TipoConsultaPessoa.cpf, cpfConsulta: pacienteResponsavelDto.ResponsavelCpf))
+                using (Pessoa paciente = await _pessoasRepository.ConsultarPessoa(tipoConsultaPessoa: TipoConsultaPessoa.cpf, cpfConsulta: pacienteResponsavelDto.PacienteCpf))
                 {
                     if (paciente is null || paciente.TipoPessoa != EnumTipoPessoa.PacienteIncapaz)
                         return NotFound("paciente com essas caracteristicas não encontrado(a)");
@@ -125,4 +125,104 @@ public class PacienteController : ControllerBase
         }
     }
 
+    [HttpPost("AssociarCuidador")]
+    public async Task<IActionResult> AssociarCuidador(AssociaPacienteCuidadorDto pacienteCuidadorDto)
+    {
+        try
+        {
+            int[] id = new int[2] { 0, 0 };
+            int[] tipoPessoa = new int[2] { 0, 0 };
+            #region Cuidador
+
+            if (pacienteCuidadorDto.CuidadorCpf != null && pacienteCuidadorDto.CuidadorId != null)
+            {
+                if (!FormataDados.VerificadorCpfPessoa(pacienteCuidadorDto.CuidadorCpf))
+                    return BadRequest("CPF inválido");
+                using (Pessoa cuidador = await _pessoasRepository.ConsultarPessoa(tipoConsultaPessoa: TipoConsultaPessoa.cpfEIdPessoa, cpfConsulta: pacienteCuidadorDto.CuidadorCpf, idPessoa: pacienteCuidadorDto.CuidadorId.ToString()))
+                {
+                    if (cuidador is null || cuidador.TipoPessoa != EnumTipoPessoa.Cuidador)
+                        return NotFound("Responsavel com essas caracteristicas não encontrado(a)");
+                    id[0] = cuidador.PessoaId;
+                    cuidador.Dispose();
+                }
+            }
+            else if (pacienteCuidadorDto.CuidadorCpf != null && pacienteCuidadorDto.CuidadorId == null)
+            {
+                if (!FormataDados.VerificadorCpfPessoa(pacienteCuidadorDto.CuidadorCpf))
+                    return BadRequest("CPF inválido");
+                using (Pessoa responsavel = await _pessoasRepository.ConsultarPessoa(tipoConsultaPessoa: TipoConsultaPessoa.cpf, cpfConsulta: pacienteCuidadorDto.CuidadorCpf))
+                {
+                    if (responsavel is null || responsavel.TipoPessoa != EnumTipoPessoa.Cuidador)
+                        return NotFound("Responsavel com essas caracteristicas não encontrado(a)");
+                    id[0] = responsavel.PessoaId;
+                    responsavel.Dispose();
+                }
+            }
+            else if (pacienteCuidadorDto.CuidadorCpf == null && pacienteCuidadorDto.CuidadorId != null)
+            {
+                using (Pessoa responsavel = await _pessoasRepository.ConsultarPessoa(tipoConsultaPessoa: TipoConsultaPessoa.pessoaId, idPessoa: pacienteCuidadorDto.CuidadorId.ToString()))
+                {
+                    if (responsavel is null || responsavel.TipoPessoa != EnumTipoPessoa.Cuidador)
+                        return NotFound("Responsavel com essas caracteristicas não encontrado(a)");
+                    id[0] = responsavel.PessoaId;
+                    responsavel.Dispose();
+                }
+            }
+            else return BadRequest("Insira todos os campos necessários");
+            #endregion
+
+            #region Paciente
+
+            if (pacienteCuidadorDto.PacienteCpf != null && pacienteCuidadorDto.PacienteId != null)
+            {
+                if (!FormataDados.VerificadorCpfPessoa(pacienteCuidadorDto.PacienteCpf))
+                    return BadRequest("CPF inválido");
+                using (Pessoa paciente = await _pessoasRepository.ConsultarPessoa(tipoConsultaPessoa: TipoConsultaPessoa.cpfEIdPessoa, cpfConsulta: pacienteCuidadorDto.PacienteCpf, idPessoa: pacienteCuidadorDto.PacienteId.ToString()))
+                {
+                    if (paciente is null || paciente.TipoPessoa != EnumTipoPessoa.PacienteIncapaz)
+                        return NotFound("paciente com essas caracteristicas não encontrado(a)");
+                    id[1] = paciente.PessoaId;
+                    paciente.Dispose();
+                }
+            }
+            else if (pacienteCuidadorDto.PacienteCpf != null && pacienteCuidadorDto.PacienteId == null)
+            {
+                if (!FormataDados.VerificadorCpfPessoa(pacienteCuidadorDto.PacienteCpf))
+                    return BadRequest("CPF inválido");
+                using (Pessoa paciente = await _pessoasRepository.ConsultarPessoa(tipoConsultaPessoa: TipoConsultaPessoa.cpf, cpfConsulta: pacienteCuidadorDto.PacienteCpf))
+                {
+                    if (paciente is null || paciente.TipoPessoa != EnumTipoPessoa.PacienteIncapaz)
+                        return NotFound("paciente com essas caracteristicas não encontrado(a)");
+                    id[1] = paciente.PessoaId;
+                    paciente.Dispose();
+
+                }
+            }
+            else if (pacienteCuidadorDto.PacienteCpf == null && pacienteCuidadorDto.PacienteId != null)
+            {
+                using (Pessoa paciente = await _pessoasRepository.ConsultarPessoa(tipoConsultaPessoa: TipoConsultaPessoa.pessoaId, idPessoa: pacienteCuidadorDto.PacienteId.ToString()))
+                {
+                    if (paciente is null || paciente.TipoPessoa != EnumTipoPessoa.PacienteIncapaz)
+                        return NotFound("paciente com essas caracteristicas não encontrado(a)");
+                    id[1] = paciente.PessoaId;
+                    paciente.Dispose();
+                }
+            }
+            else return BadRequest("Insira todos os campos necessários");
+            #endregion
+
+            CuidadorPaciente CuidadorPaciente = new()
+            {
+                CuidadorId = id[0],
+                PacienteId = id[1],
+                CriadoEm = DateTime.Now,
+            };
+
+            return await _pacienteRepository.SaveCuidadorPaciente(CuidadorPaciente) ? Ok("Cuidador adicionado com sucesso") : BadRequest("Duplicidade invalida");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
 }
