0942c35ce41159dbd1ca7d1f493839c4c68f21f0
DanMarzo <marzogildan@gmail.com>
2023-04-23 22:28:13 -0300

    Incluindo novos contratos

---
+++ WEB_API_HealTime/Repository/Interfaces/IMedicacaoRepository.cs
@@ -0,0 +1,13 @@
+﻿using WEB_API_HealTime.Models.ConsultasMedicas;
+using WEB_API_HealTime.Models.Medicacoes;
+
+namespace WEB_API_HealTime.Repository.Interfaces;
+
+public interface IMedicacaoRepository
+{
+    Task<List<Medico>> ListarMedicos();
+    Task<bool> MedicoExiste(int id);
+    Task<int> IncluiPrescricaoPaciente(PrescricaoPaciente prescricaoPaciente);
+    Task<bool> MedicacaoExiste(int id);
+    Task<bool> IncluiPrescricaoMedicacao(List<PrescricaoMedicacao> listPrescricaoMedicacaos);
+}
