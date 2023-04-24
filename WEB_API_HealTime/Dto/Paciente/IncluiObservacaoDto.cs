a2759915c7925c7f39ca1841b378cf3ad450a911
DanMarzo <marzogildan@gmail.com>
2023-04-23 22:27:51 -0300

    exclusao do desnecessario

--- WEB_API_HealTime/Dto/Paciente/IncluiObservacaoDto.cs
+++ WEB_API_HealTime/Dto/Paciente/IncluiObservacaoDto.cs
@@ -4,9 +4,7 @@ namespace WEB_API_HealTime.Dto.Paciente;
 
 public class IncluiObservacaoDto
 {
-    public int SqObservacao { get; set; }
     [Required]
     public int PacienteId { get; set; }
-    public DateTime MtObservacao { get; set; }
     public string Observacao { get; set; }
 }
