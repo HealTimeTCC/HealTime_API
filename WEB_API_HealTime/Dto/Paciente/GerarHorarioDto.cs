476d06a09a5a1be4a6f0860203c6755467b87088
DanMarzo <marzogildan@gmail.com>
2023-04-30 19:00:11 -0300

    Novos dto para puxar

---
+++ WEB_API_HealTime/Dto/Paciente/GerarHorarioDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WEB_API_HealTime.Dto.Paciente;
+
+public class GerarHorarioDto
+{ 
+    public int PrescricaoPacienteId     { get; set; }
+    public int PrescricaoMedicamentoId  { get; set; }
+    public int MedicamentoId { get; set; }
+}
