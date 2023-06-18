8313c817254c440cbc5d4654c38a36089b21a8d3
DanMarzo <marzogildan@gmail.com>
2023-06-18 18:09:46 -0300

    Consertando Trigger, a mesma esta funcional

--- Querry/script_de_depencias_database.sql
+++ Querry/script_de_depencias_database.sql
@@ -8,29 +8,32 @@ AS
 BEGIN
 	IF(EXISTS(SELECT 1 FROM inserted) AND EXISTS(SELECT 1 FROM deleted))
 	BEGIN
-		DECLARE @QTD_ATUAL INT, @COD_ANDAMENTO INT, @QTD_CONTROLE_ESTADO INT;
-		SELECT @COD_ANDAMENTO = AndamentoMedicacaoId FROM inserted
+		DECLARE 
+			@QTD_ATUAL INT
+			, @COD_PRESCRICAO_MEDICACAO INT
+			, @QTD_CONTROLE_ESTADO INT
+			, @COD_PRESCRICAO_PACIENTE INT;
+
+		SELECT 
+			@COD_PRESCRICAO_MEDICACAO = PrescricaoMedicacaoId
+			, @COD_PRESCRICAO_PACIENTE = PrescricaoPacienteId 
+		FROM inserted;
+
 		SELECT @QTD_ATUAL = COUNT(*) FROM AndamentoMedicacoes 
-			WHERE BaixaAndamentoMedicacao = 1 
-			AND AndamentoMedicacaoId = @COD_ANDAMENTO;
+			WHERE BaixaAndamentoMedicacao = 1 AND PrescricaoMedicacaoId = @COD_PRESCRICAO_MEDICACAO;
 
 		SELECT @QTD_CONTROLE_ESTADO = QtdeHorarios 
 			FROM ControleEstadoAndamentoMedicacao
-			WHERE CodPrescricaoMedicamento = @COD_ANDAMENTO
+			WHERE CodPrescricaoMedicamento = @COD_PRESCRICAO_MEDICACAO
 		
 		IF(@QTD_ATUAL = @QTD_CONTROLE_ESTADO)
 		BEGIN
+			--Muda o status da Prescriçao Para Inativo
 			UPDATE PrescricoesMedicacoes SET StatusMedicacaoFlag = 0  
-			WHERE PrescricaoMedicacaoId = (
-			SELECT TOP 1 PrescricaoMedicacaoId 
-				FROM AndamentoMedicacoes 
-			WHERE AndamentoMedicacaoId = @COD_ANDAMENTO
-			);
+			WHERE PrescricaoMedicacaoId = @COD_PRESCRICAO_MEDICACAO;
+
 			UPDATE PrescricaoPacientes SET FlagStatusAtivo = 0  
-			WHERE PrescricaoPacienteId = (
-			SELECT TOP 1 PrescricaoPacienteId FROM AndamentoMedicacoes 
-			WHERE AndamentoMedicacaoId = @COD_ANDAMENTO
-			);
+			WHERE PrescricaoPacienteId = @COD_PRESCRICAO_PACIENTE;
 		END
 	END
 END
